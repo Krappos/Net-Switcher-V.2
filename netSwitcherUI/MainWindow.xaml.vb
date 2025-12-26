b84832296319b993e4c05b875ee2cc7e6825a44c
Krapposko <akopaksk@gmail.com>
2025-12-26 02:16:47 +0100

    added a logic for enabling & disabling proxy

--- netSwitcherUI/MainWindow.xaml.vb
+++ netSwitcherUI/MainWindow.xaml.vb
@@ -1,12 +1,12 @@
 ﻿Imports System.Collections.ObjectModel
-Imports Wpf.Ui.Controls
-Imports netSwitcherBackend
 Imports System.IO
+Imports System.Net.Mime.MediaTypeNames
+Imports netSwitcherBackend
+Imports Wpf.Ui.Controls
 
 
 Class MainWindow
     Inherits FluentWindow
-
 
     Public Property Items As New ObservableCollection(Of NetworkInfo)()
 
@@ -14,13 +14,11 @@ Class MainWindow
     Dim informacia As New NetworkInfo()
     Dim file As New ProxyWorker()
     Dim activeState As Boolean
+    Dim proxy As New ProxyController()
 
     Public Sub New()
         InitializeComponent()
-
         firstBoot()
-
-
 
         Me.DataContext = Me
     End Sub
@@ -30,12 +28,12 @@ Class MainWindow
         If CheckBox1.IsChecked = True Then
             Tb_script.IsEnabled = True
             Tb_script.TextDecorations = Nothing
+            proxy.EnableProxyScript()
         Else
             Tb_script.IsEnabled = False
             Tb_script.TextDecorations = TextDecorations.Strikethrough
+            proxy.DisableProxyScript()
         End If
-
-
 
     End Sub
 
@@ -50,12 +48,19 @@ Class MainWindow
 
     Private Sub firstBoot()
         file.CreateFiles()
-        Tb_script.Text = file.ReadScript()
+        ProxyStatus()
         loadNetworkInfo()
         CheckBox1.IsChecked = True
 
+    End Sub
+    Private Sub ProxyStatus()
+        If CheckBox1.IsChecked Then
+            proxy.EnableProxyScript()
 
+        Else
 
+            proxy.DisableProxyScript()
+        End If
     End Sub
     Private Sub loadNetworkInfo()
 
@@ -70,15 +75,18 @@ Class MainWindow
         file.WriteScript(Tb_script.Text)
         MsgBox("vasa proxy je uspesne zmenená")
     End Sub
+
 #End Region
 
 
 End Class
 
 'nacitavanie sieti do zoznamu funguje <3
+'upravit proxy font a textbox <3
+'pridanie kniznice na zapis scriptu do systemu<3
+'pridat logiku checkboxu a textboxu na script <
 
 
 'pridat bluetooth  icon 
-'upravit proxy font a textbox 
-'pridat logiku checkboxu a textboxu na script
-'pridanie kniznice na zapis scriptu do systemu
+
+'pridanie logiky na aktivnu siet
